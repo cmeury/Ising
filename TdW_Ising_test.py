f902a111ca877f06e6c005c54e6b111a3b0f3b9c
Cedric Meury <cedric@meury.com>
2013-05-28 17:49:53 +0200

    unit testing Spin.spin()

---
+++ TdW_Ising_test.py
@@ -0,0 +1,37 @@
+import unittest
+from numpy import array, testing
+from TdW_Ising import Spin
+
+class TestTdWIsing(unittest.TestCase):
+
+	def setUp(self):
+		self.spin = Spin(array([1,1,-1,-1]),0)
+
+	def test_flip_first(self):
+		flipped_spins = self.spin.flip(0)
+		print self.spin.spins
+		testing.assert_array_equal([-1,1,-1,-1], flipped_spins)
+
+	def test_flip_last(self):
+		flipped_spins = self.spin.flip(3)
+		testing.assert_array_equal([1,1,-1,1], flipped_spins)
+
+	def test_flip_middle(self):
+		flipped_spins = self.spin.flip(1)
+		testing.assert_array_equal([1,-1,-1,-1], flipped_spins)
+
+	def test_flip_single(self):
+		spin = Spin(array([1]),0)
+		flipped_spins = spin.flip(0)
+		testing.assert_array_equal([-1], flipped_spins)
+
+	def test_flip_empty(self):
+		spin = Spin(array([]),0)
+		flipped_spins = self.spin.flip(0)
+		testing.assert_array_equal([], flipped_spins)
+
+	def test_flip_outofbounds(self):
+		self.assertRaises(IndexError, self.spin.flip, 5)
+
+if __name__ == '__main__':
+	unittest.main()
